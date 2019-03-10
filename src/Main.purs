a4e23ccc404209d26d901b084d5f1f2e2397f6ea
bouzuya <m@bouzuya.net>
2019-03-10 13:56:42 +0900

    show indexContent

--- src/Main.purs
+++ src/Main.purs
@@ -9,33 +9,52 @@ import Data.Array as Array
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console as Console
+import Foreign.Object (Object)
 import Foreign.Object as Object
 import Node.FS.Sync as FS
 import Node.Process as Process
 
-template :: String
-template =
-  Array.intercalate
-    "\n"
-    [ "import { Test } from 'beater';"
-    , "{{importTests}}"
-    , ""
-    , "const tests = ([] as Test[])"
-    , "{{concatTests}};"
-    , ""
-    , "export { tests };"
-    ]
-
 indexContent :: Array String -> String
-indexContent _ =
-  TemplateString.template
-    template
-    (Object.fromFoldable
-      [ -- FIXME
-        Tuple "importTests" "import { tests as fooTests } from './foo';"
-        -- FIXME
-      , Tuple "concatTests" "  .concat(fooTests)"
-      ])
+indexContent files = TemplateString.template template variables
+  where
+    concatTests :: String
+    concatTests = lines "  .concat({{name}}Tests)" files
+
+    fileTemplate :: String -> String -> String
+    fileTemplate t f =
+      TemplateString.template
+        t
+        (Object.fromFoldable
+          [ Tuple "file" f
+          , Tuple "name" f -- FIXME
+          ])
+
+    importTests :: String
+    importTests =
+      lines "import { tests as {{name}}Tests } from './{{file}}';" files
+
+    lines :: String -> Array String -> String
+    lines t fs = Array.intercalate "\n" (map (fileTemplate t) fs)
+
+    template :: String
+    template =
+      Array.intercalate
+        "\n"
+        [ "import { Test } from 'beater';"
+        , "{{importTests}}"
+        , ""
+        , "const tests = ([] as Test[])"
+        , "{{concatTests}};"
+        , ""
+        , "export { tests };"
+        ]
+
+    variables :: Object String
+    variables =
+      Object.fromFoldable
+        [ Tuple "importTests" importTests
+        , Tuple "concatTests" concatTests
+        ]
 
 main :: Effect Unit
 main = do
