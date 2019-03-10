8eda95b1e8f1129c35da55f9c1a8e16c2706111a
bouzuya <m@bouzuya.net>
2019-03-10 13:17:24 +0900

    show args

--- spago.dhall
+++ spago.dhall
@@ -5,7 +5,7 @@ You can edit this file as you like.
 { name =
     "my-project"
 , dependencies =
-    [ "console", "effect", "psci-support", "test-unit" ]
+    [ "console", "effect", "node-process", "psci-support", "test-unit" ]
 , packages =
     ./packages.dhall
 }
--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,12 @@ module Main
 
 import Prelude
 
+import Data.Array as Array
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console as Console
+import Node.Process as Process
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  args <- map (Array.drop 2) Process.argv
+  Console.logShow args
